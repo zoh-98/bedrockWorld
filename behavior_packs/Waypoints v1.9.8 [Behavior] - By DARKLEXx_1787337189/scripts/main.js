4c7bc0745aac90565b058f82e8c2d53661f5da0c
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2026-08-22 16:18:44 +0100

    Bedrock backup 2026-08-22T15:18:44.361Z

---
+++ behavior_packs/Waypoints v1.9.8 [Behavior] - By DARKLEXx_1787337189/scripts/main.js
@@ -0,0 +1,44 @@
+/*
+.
+.
+Thanks for downloading "Vanilla Waypoints", version: 1.9.8!
+Creator: DARKLEXx
+.
+This add-on was made with love for you!
+Enjoy it in your Minecraft Bedrock world, server, or realm!
+.
+.
+Contact, Social & Support:
+- Discord Community: https://discord.com/invite/PPT2uQSvpY
+- YouTube: https://www.youtube.com/@DARKLEXx_mc
+- Buy Me A Coffee: https://buymeacoffee.com/darklexx
+- CurseForge: https://www.curseforge.com/members/darklexx/projects
+- MCPEDL: https://mcpedl.com/user/darklexx/
+- Discord: @darklexx_mc
+- Email: darklexx.minecraft@gmail.com
+.
+.
+You ARE permitted to:
+- Use the add-on in your Minecraft worlds, servers, or realms.
+- Make a video, review, or something like that, but ONLY include the official download links from CurseForge or MCPEDL (If you include links to your websites, the FINAL DOWNLOAD page must be the official sites).
+- Modify the code ONLY for yourself and your world; you cannot publish it! (You need my permission to know how the add-on works).
+.
+You are NOT permitted to:
+- Claim the code as your own.
+- Publish the addon as your own.
+- Modify and redistribute without DARKLEXx permission.
+.
+.
+.
+.
+.
+*/
+import './newConfigs/configs.js';
+
+/*
+.
+Obfuscated with DL's Obfuscator Software v3.5.0
+I hope you enjoy my work :)
+Thanks for playing my addon!
+.
+*/
